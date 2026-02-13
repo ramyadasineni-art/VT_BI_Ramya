7f23efb34eed2bb5ff5eafad32702d53879ea3a7
Aravind Reddy <aravindreddy.dondapati@votarytech.com>
2026-02-13 12:38:36 +0530

    addiding new lines in the addition.c file

--- addition.c
+++ addition.c
@@ -2,7 +2,9 @@
 #include<stdio.h>
 int main()
 {
-	int a = 10, b = 20;
+	int a, b;
+	printf("Enter a and b values: ");
+	scanf("%d %d", &a, &b);
 	int add = a + b;
 	printf("Result : %d\n", add);
 	return 0;
