e5e467e22127958f8ff0497681bd203e8e78f9ab
Bindu <bindumadhavi.bollula@votarytech.com>
2026-02-16 16:27:57 +0530

    latest amend

--- sum.c
+++ sum.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int a=5,b=8,sum=0;
+	int a=10,b=2,sum=0;
 	sum=a+b;
 	printf("sum=%d\n",sum);
-	printf("product=%d",a*b);
+	printf("product=%d\n",a*b);
+	printf("division=%d",a/b);
 }
 	
 
