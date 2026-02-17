eb38f066bccd3e329f8f0705d871548ddbae263e
Bindu <bindumadhavi.bollula@votarytech.com>
2026-02-17 14:48:33 +0530

    factorial using recurrsion is done

--- factorial.c
+++ factorial.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
+int fact(int num)
+{
+	if(num==1)
+		return 1;
+	else
+		return (num*(fact(num-1)));
+}
 int main()
 {
 	int num,prod=1;
 	printf("enter the num\n");
 	scanf("%d",&num);
-	for(int i=1;i<=num;i++)
+	/*for(int i=1;i<=num;i++)
 	{
 		prod=prod*i;
 	}
-	printf("fact=%d\n",prod);
+	printf("fact=%d\n",prod);*/
+	printf("fact=%d\n",fact(num));
 }
