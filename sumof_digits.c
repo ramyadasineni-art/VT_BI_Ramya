a47e8c4380ac4ce3b827f66f350e2bf52e464506
Bindu <bindumadhavi.bollula@votarytech.com>
2026-02-18 14:15:42 +0530

    product of digits is done

--- sumof_digits.c
+++ sumof_digits.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int num,digit,sum=0;
+	int num,digit,sum=0,prod=1;
 	printf("enter the number\n");
 	scanf("%d",&num);
         while(num>0)
 	{
 		digit=num%10;
 		sum=sum+digit;
+		prod=prod*digit;
 		num/=10;
+
 	}
 	printf("sum=%d",sum);
+	printf("prod=%d",prod);
 }
