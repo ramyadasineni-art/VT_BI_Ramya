28a264af3674eca645ab3ed7932951d3bd74ffcd
Bindu <bindumadhavi.bollula@votarytech.com>
2026-02-23 18:09:11 +0530

    string reverse program is done

---
+++ string_reverse.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+char* str_reverse(char *str)
+{
+	char *left,*right,temp;
+	int len=strlen(str)-1;
+	left=str;
+	right=str+len;
+	while(left<right)
+	{
+		temp=*left;
+		*left=*right;
+		*right=temp;
+		left++,right--;
+	}
+	return str;
+}
+
+int main()
+{
+	char str[10];
+	printf("enetr the string\n");
+	scanf("%s",str);
+	str_reverse(str);
+	printf("%s",str);
+}
