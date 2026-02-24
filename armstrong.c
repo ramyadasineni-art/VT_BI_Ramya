90ed0f8dfeac2d3b537fe3099bc7b24382c6ee7a
Laxmiprasanna pateti <laxmiprasanna.pateti@votarytech.com>
2026-02-24 11:15:32 +0530

    delete armstrong

--- armstrong.c
+++ armstrong.c
@@ -1,17 +1,1 @@
-#include<stdio.h>
-int main()
-{
-	int num,temp,arm=0,rem=0;
-	printf("enter number:\n");
-	scanf("%d",&num);
-	temp=num;
-	while(temp){
-		rem=temp%10;
-		arm=arm+(rem*rem*rem);
-		temp/=10;
-	}
-	if(num==arm)
-		printf("armstrong number\n");
-	else
-		printf("not armstrong\n");
-}
+
